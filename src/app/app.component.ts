21b3e066f64f7ac309d8e68a42d835ed0e909888
Avtandil-1 <avtandil.merebashvili.2@gmail.com>
2024-01-12 14:31:05 +0400

    add snackbar message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { BehaviorSubject, filter, map, mergeMap, of, take, tap } from 'rxjs';
 import { SharedModule } from './common/shared.module';
 import { quizContent } from './quiz/data/content';
 import { QuizService } from './quiz/services/quiz.service';
+import { ITask } from './quiz/common/interface/generate-quiz.interface';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SnackbarComponent } from './common/components/snackbar/snackbar.component';
 
 @Component({
   selector: 'app-root',
@@ -19,7 +22,11 @@ export class AppComponent implements OnInit {
   public showSpinner$ = this.tasks$.pipe(map((tasks) => tasks.length));
   public progess$ = new BehaviorSubject(0);
 
-  constructor(private quizService: QuizService<any>, private router: Router) {}
+  constructor(
+    private quizService: QuizService<any>,
+    private router: Router,
+    private matSnackbar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.initNavigation();
@@ -44,7 +51,7 @@ export class AppComponent implements OnInit {
               next ? tasks[tasks.length - 1].id != tasks[currentIdx].id : true
             ),
             filter((currentIdx) =>
-              next ? this.quizService.existsInStore(tasks[currentIdx].id) : true
+              this.handleNavigationNext(next, tasks, currentIdx)
             ),
             map((currentIdx) => tasks[currentIdx + (next ? 1 : -1)]),
             filter((newTask) => Boolean(newTask) != false),
@@ -72,4 +79,21 @@ export class AppComponent implements OnInit {
         )
       )
       .subscribe();
+
+  private handleNavigationNext(
+    next: boolean,
+    tasks: ITask[],
+    currentIdx: number
+  ) {
+    if (!next) return true;
+    const answered = this.quizService.existsInStore(tasks[currentIdx].id);
+    if (answered) {
+      return true;
+    }
+    this.matSnackbar.openFromComponent(SnackbarComponent, {
+      data: 'შეიყვანეთ პასუხი',
+      duration: 2000,
+    });
+    return false;
+  }
 }
